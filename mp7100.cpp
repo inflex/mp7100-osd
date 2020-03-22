18cbb0b5dfabd1e0e0b36b3691e8423974ca9a33
Inflex <pldaniels@gmail.com>
2020-03-23 01:15:51 +1000

    Added serial port capability for linux; ./mp7100 -p /dev/ttyUSB0 -s 115200:8:n '

--- mp7100.cpp
+++ mp7100.cpp
@@ -56,14 +56,41 @@
 
 #define MMFLAG_AUTORANGE	0b01000000
 
+#define CMODE_USB 1
+#define CMODE_SERIAL 2
+#define CMODE_NONE 0
+
+#define MEAS_VOLT "MEAS:VOLT?"
+#define MEAS_CURR "MEAS:CURR?"
+
 char SEPARATOR_DP[] = ".";
+
+struct serial_params_s {
+	char *device;
+	int fd, n;
+	int cnt, size, s_cnt;
+	struct termios oldtp, newtp;
+};
+
 
 struct glb {
 	uint8_t debug;
 	uint8_t quiet;
 	uint16_t flags;
+	uint16_t error_flag;
 	char *output_file;
 	char *device;
+
+	char meas_volt[20];
+	char meas_curr[20];
+
+	int usb_fhandle;
+
+	int comms_mode;
+	char *com_address;
+	char *serial_parameters_string; // this is the raw from the command line
+	struct serial_params_s serial_params; // this is the decoded version
+
 
 	int interval;
 	int font_size;
@@ -138,9 +165,13 @@ int init(struct glb *g) {
 	g->debug = 0;
 	g->quiet = 0;
 	g->flags = 0;
+	g->error_flag = 0;
 	g->output_file = NULL;
 	g->interval = 100000;
 	g->device = NULL;
+	g->comms_mode = CMODE_NONE;
+
+	g->serial_parameters_string = NULL;
 
 	g->font_size = 60;
 	g->window_width = 400;
@@ -171,6 +202,8 @@ void show_help(void) {
 			"\t-ca <amps colour, ffffa0>\r\n"
 			"\t-cb <background colour, 101010>\r\n"
 			"\t-t <interval> (sleep delay between samples, default 100,000us)\r\n"
+			"\t-p <comport>: Set the com port for the meter, eg: -p /dev/ttyUSB0\r\n"
+			"\t-s <[9600|4800|2400|1200]:[7|8][o|e|n][1|2]>, eg: -s 2400:8n1\r\n"
 			"\r\n"
 			"\texample: MP7100 -p /dev/usbtmc2\r\n"
 			, BUILD_VER
@@ -307,6 +340,11 @@ int parse_parameters(struct glb *g, int argc, char **argv ) {
 							 }
 							 break;
 
+				case 's':
+							 i++;
+							 g->serial_parameters_string = argv[i];
+							 break;
+
 				default: break;
 			} // switch
 		}
@@ -316,6 +354,202 @@ int parse_parameters(struct glb *g, int argc, char **argv ) {
 }
 
 
+
+/*
+ * Default parameters are 2400:8n1, given that the multimeter
+ * is shipped like this and cannot be changed then we shouldn't
+ * have to worry about needing to make changes, but we'll probably
+ * add that for future changes.
+ *
+ */
+void open_port( struct glb *g ) {
+#ifdef __linux__
+	struct serial_params_s *s = &(g->serial_params);
+	char *p = g->serial_parameters_string;
+	char default_params[] = "9600:8:n";
+	int r; 
+
+	if (!p) p = default_params;
+
+	fprintf(stdout,"Attempting to open '%s'\n", s->device);
+	s->fd = open( s->device, O_RDWR | O_NOCTTY | O_NDELAY );
+	if (s->fd <0) {
+		perror( s->device );
+	}
+
+	fcntl(s->fd,F_SETFL,0);
+	tcgetattr(s->fd,&(s->oldtp)); // save current serial port settings 
+	tcgetattr(s->fd,&(s->newtp)); // save current serial port settings in to what will be our new settings
+	cfmakeraw(&(s->newtp));
+	
+		s->newtp.c_cflag = CS8 |  CLOCAL | CREAD ; 
+
+      if (strncmp(p, "115200:", 7) == 0) s->newtp.c_cflag |= B115200; 
+		else if (strncmp(p, "57600:", 6) == 0) s->newtp.c_cflag |= B57600;
+		else if (strncmp(p, "38400:", 6) == 0) s->newtp.c_cflag |= B38400;
+		else if (strncmp(p, "19200:", 6) == 0) s->newtp.c_cflag |= B19200;
+		else if (strncmp(p, "9600:", 5) == 0) s->newtp.c_cflag |= B9600;
+		else if (strncmp(p, "4800:", 5) == 0) s->newtp.c_cflag |= B4800;
+      else if (strncmp(p, "2400:", 5) == 0) s->newtp.c_cflag |= B2400; //
+      else {
+         fprintf(stdout,"Invalid serial speed\r\n");
+         exit(1);
+      }
+
+		p = strchr(p,':');
+		if (p) {
+			p++;
+			// we don't do anything with the data bits because
+			// this PSU only accepts 8, so just jump to the next
+			// field
+			p = strchr(p,':');
+			if (!p) {
+				fprintf(stdout,"Invalid serial format string\n");
+				exit(1);
+			}
+      }
+
+      p++;
+      if (*p == 'o') s->newtp.c_cflag |= PARODD;
+      else if (*p == 'e') s->newtp.c_cflag |= PARENB;
+      else if (*p == 'n') s->newtp.c_cflag &= ~(PARODD|PARENB);
+      else {
+         fprintf(stdout,"Invalid serial parity type '%c'\r\n", *p);
+         exit(1);
+      }
+
+	s->newtp.c_iflag &= ~(IXON | IXOFF | IXANY );
+
+	r = tcsetattr(s->fd, TCSANOW, &(s->newtp));
+	if (r) {
+		fprintf(stderr,"%s:%d: Error setting terminal (%s)\n", FL, strerror(errno));
+		exit(1);
+	}
+
+	fprintf(stdout,"Serial port opened, FD[%d]\n", s->fd);
+#endif
+}
+
+uint8_t a2h( uint8_t a ) {
+	a -= 0x30;
+	if (a < 10) return a;
+	a -= 7;
+	return a;
+}
+
+int data_read( glb *g, char *b, ssize_t s ) {
+	ssize_t sz;
+	if (g->comms_mode == CMODE_USB) {
+		/*
+		 * usb mode read
+		 *
+		 */
+		int bp = 0;
+		do {
+			sz = read(g->usb_fhandle, b+bp, s -1 -bp);
+			b[bp+sz] = '\0';
+
+			if (sz == -1) {
+				g->error_flag = true;
+				fprintf(stdout,"Error reading data: %s\n", strerror(errno));
+				snprintf(b, s, "NODATA");
+				break;
+			}
+
+			bp += sz;
+			if (sz == 0) break;
+			if (bp >= s) break;
+			usleep(1000);
+		} while (sz);
+		b[bp] = '\0';
+		if ((bp > 0) && b[bp-1] == '\n') b[bp -1] = '\0';
+
+	} else {
+		/*
+		 * serial mode read
+		 *
+		 */
+		int bp = 0;
+		ssize_t bytes_read = 0;
+
+		do {
+			char temp_char;
+			bytes_read = read(g->serial_params.fd, &temp_char, 1);
+			if (bytes_read) {
+				b[bp] = temp_char;
+				if (b[bp] == '\n') break;
+				bp++;
+			}
+		} while (bytes_read && bp < s);
+		b[bp] = '\0';
+	}
+	return sz;
+}
+
+int data_write( glb *g, char *d, ssize_t s ) { 
+		ssize_t sz;
+
+	if (g->comms_mode == CMODE_USB) {
+		/*
+		 * usb mode write
+		 *
+		 */
+		sz = write(g->usb_fhandle, d, s); //"MEAS:VOLT?", sizeof("MEAS:VOLT?"));
+		if (sz < 0) {
+			g->error_flag = true;
+			fprintf(stdout,"Error sending USB data: %s\n", strerror(errno));
+			snprintf(d,s-1,"NODATA");
+			//exit(1);
+		}
+	} else {
+		/*
+		 * serial mode write
+		 *
+		 */
+		sz = write(g->serial_params.fd, d, s); 
+		if (sz < 0) {
+			g->error_flag = true;
+			fprintf(stdout,"Error sending serial data: %s\n", strerror(errno));
+			snprintf(d,s-1,"NODATA");
+		}
+	}
+
+	return sz;
+}
+
+#ifdef __WIN32
+void parse_serial_parameters( struct glb *g ) {
+      char *p = g->serial_parameters_string;
+      if (strncmp(p, "9600:", 5) == 0) dcbSerialParams.BaudRate = CBR_9600; // BaudRate = 9600
+      else if (strncmp(p, "4800:", 5) == 0) dcbSerialParams.BaudRate = CBR_4800; // BaudRate = 4800
+      else if (strncmp(p, "2400:", 5) == 0) dcbSerialParams.BaudRate = CBR_2400; // BaudRate = 2400
+      else if (strncmp(p, "1200:", 5) == 0) dcbSerialParams.BaudRate = CBR_1200; // BaudRate = 1200
+      else {
+         wprintf(L"Invalid serial speed\r\n");
+         CloseHandle(hComm);
+         exit(1);
+      }
+
+      p = &(pg->serial_params[5]);
+      if (*p == '7') dcbSerialParams.ByteSize = 7;
+      else if (*p == '8') dcbSerialParams.ByteSize = 8;
+      else {
+         wprintf(L"Invalid serial byte size '%c'\r\n", *p);
+         CloseHandle(hComm);
+         exit(1);
+      }
+
+      p++;
+      if (*p == 'o') dcbSerialParams.Parity = ODDPARITY;
+      else if (*p == 'e') dcbSerialParams.Parity = EVENPARITY;
+      else if (*p == 'n') dcbSerialParams.Parity = NOPARITY;
+      else {
+         wprintf(L"Invalid serial parity type '%c'\r\n", *p);
+         CloseHandle(hComm);
+         exit(1);
+      }
+}
+#endif
 
 /*-----------------------------------------------------------------\
   Date Code:	: 20180127-220307
@@ -342,17 +576,11 @@ int main ( int argc, char **argv ) {
 
 	char linetmp[SSIZE]; // temporary string for building main line of text
 
-//	uint8_t d[SSIZE];
-//	uint8_t dt[SSIZE];      // Serial data packet
-//	int dt_loaded = 0;	// set when we have our first valid data
-//	uint8_t dps = 0;     // Number of decimal places
 	struct glb g;        // Global structure for passing variables around
 	int i = 0;           // Generic counter
 	char temp_char;        // Temporary character
 	char tfn[4096];
 	bool quit = false;
-	bool error_flag = false;
-	int f;
 
 	glbs = &g;
 
@@ -370,6 +598,23 @@ int main ( int argc, char **argv ) {
 		exit(1);
 	}
 
+	fprintf(stdout,"START\n");
+
+	if (strstr(g.device,"usbtmc")) {
+		fprintf(stdout,"\nUsing USB mode\n\n");
+		fflush(stdout);
+		g.comms_mode = CMODE_USB;
+		snprintf(g.meas_volt,sizeof(g.meas_volt),"%s", MEAS_VOLT);
+		snprintf(g.meas_curr,sizeof(g.meas_curr),"%s", MEAS_CURR);
+	} else {
+		fprintf(stdout,"\nUsing SERIAL mode\n\n");
+		fflush(stdout);
+		g.comms_mode = CMODE_SERIAL;
+		g.serial_params.device = g.device;
+		snprintf(g.meas_volt,sizeof(g.meas_volt),"%s\n", MEAS_VOLT);
+		snprintf(g.meas_curr,sizeof(g.meas_curr),"%s\n", MEAS_CURR);
+	}
+
 	/* 
 	 * check paramters
 	 *
@@ -379,14 +624,25 @@ int main ( int argc, char **argv ) {
 
 	if (g.output_file) snprintf(tfn,sizeof(tfn),"%s.tmp",g.output_file);
 
-	/*
-	 * Handle the USB port
-	 */
 
-	f = open( g.device, O_RDWR );
-	if (f == -1) {
-		fprintf(stdout, "Error opening device [%s] : %s\n", g.device, strerror(errno));
-		exit (1);
+	if ( g.comms_mode == CMODE_SERIAL ) {
+		/* 
+		 * handle the serial port
+		 *
+		 */
+		open_port( &g );
+
+	} else {
+		/*
+		 * Handle the USB port
+		 *
+		 */
+
+		g.usb_fhandle = open( g.device, O_RDWR );
+		if (g.usb_fhandle == -1) {
+			fprintf(stdout, "Error opening device [%s] : %s\n", g.device, strerror(errno));
+			exit (1);
+		}
 	}
 
 	/*
@@ -460,7 +716,8 @@ int main ( int argc, char **argv ) {
 		linetmp[0] = '\0';
 
 
-		if (error_flag) {
+		/*
+		if (g.error_flag) {
 			f = open( g.device, O_RDWR );
 			if (f == -1) {
 				fprintf(stdout, "Error opening device [%s] : %s\n", g.device, strerror(errno));
@@ -470,67 +727,16 @@ int main ( int argc, char **argv ) {
 			}
 
 		}
+		*/
 
-		sz = write(f, "MEAS:VOLT?", sizeof("MEAS:VOLT?"));
-		if (sz < 0) {
-			error_flag = true;
-			fprintf(stdout,"Error sending VOLT query: %s\n", strerror(errno));
-			snprintf(buf_volt,sizeof(buf_volt),"NODATA");
-			//exit(1);
-		}
+		sz = data_write( &g, g.meas_volt, strlen(g.meas_volt)  );
 		usleep(20000);
+		sz = data_read( &g, buf_volt, sizeof(buf_volt) );
 
-		int bp = 0;
-		do {
-			sz = read(f, buf_volt +bp, sizeof(buf_volt) -1 -bp);
-			buf_volt[bp+sz] = '\0';
-//			fprintf(stdout,"%s|", buf_volt+bp);
 
-			if (sz == -1) {
-			error_flag = true;
-				fprintf(stdout,"Error reading data from PSU: %s\n", strerror(errno));
-				snprintf(buf_volt,sizeof(buf_volt),"NODATA");
-				break;
-				//exit(1);
-			}
-
-			bp += sz;
-			if (sz == 0) break;
-			if (bp >= sizeof(buf_volt)) break;
-			usleep(1000);
-		} while (sz);
-		buf_volt[bp] = '\0';
-		if ((bp > 0) && buf_volt[bp-1] == '\n') buf_volt[bp -1] = '\0';
-
-		bp = 0;
-		sz = write(f, "MEAS:CURR?", sizeof("MEAS:CURR?"));
-		if (sz < 0) {
-			error_flag = true;
-			fprintf(stdout,"Error sending CURRENT query: %s\n", strerror(errno));
-			snprintf(buf_volt,sizeof(buf_volt),"NODATA");
-			//exit(1);
-		}
+		sz = data_write( &g, g.meas_curr, strlen(g.meas_curr));
 		usleep(20000);
-		do {
-			sz = read(f, buf_curr+bp, sizeof(buf_curr) -1 -bp);
-			if (sz == -1) {
-				error_flag = true;
-				fprintf(stdout,"Error reading data from PSU: %s\n", strerror(errno));
-				snprintf(buf_volt,sizeof(buf_volt),"NODATA");
-				break;
-				//exit(1);
-			}
-
-			buf_curr[bp+sz] = '\0';
-//			fprintf(stdout,"%s|", buf_curr+bp);
-			bp += sz;
-
-			if (sz == 0) break;
-			if (bp >= sizeof(buf_curr)) break;
-			usleep(1000);
-		} while (sz);
-		buf_curr[bp] = '\0';
-		if ((bp > 0) && buf_curr[bp-1] == '\n') buf_curr[bp -1] = '\0';
+		sz = data_read( &g, buf_curr, sizeof(buf_curr));
 
 		/*
 		 *
@@ -540,8 +746,8 @@ int main ( int argc, char **argv ) {
 
 
 		//snprintf(line1, sizeof(line1)-1, "%s%s", buf_volt, error_flag?"":"V");
-		snprintf(line1, sizeof(line1), "%7s%s", buf_volt, error_flag?"":"V");
-		snprintf(line2, sizeof(line2), "%7s%s", buf_curr, error_flag?"":"A");
+		snprintf(line1, sizeof(line1), "%7s%s", buf_volt, g.error_flag?"":"V");
+		snprintf(line2, sizeof(line2), "%7s%s", buf_curr, g.error_flag?"":"A");
 		if (g.debug) fprintf(stdout,"%s\n%s\n", line1, line2);
 
 
@@ -572,11 +778,11 @@ int main ( int argc, char **argv ) {
 				SDL_FreeSurface(surface_2);
 			}
 
-		if (error_flag) {
-			sleep(1);
-		} else {
-			usleep(g.interval);
-		}
+			if (g.error_flag) {
+				sleep(1);
+			} else {
+				usleep(g.interval);
+			}
 
 
 		}
@@ -603,7 +809,9 @@ int main ( int argc, char **argv ) {
 
 	} // while(1)
 
-	close(f);
+	if (g.comms_mode == CMODE_USB) {
+		close(g.usb_fhandle);
+	}
 
 	TTF_CloseFont(font);
 	SDL_DestroyRenderer(renderer);
